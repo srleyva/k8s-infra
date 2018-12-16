13cc4b9f3df9ce29d8eee49e5f09108bf23e3e2d
Stephen Leyva <sleyva@staq.com>
2018-12-16 16:34:13 -0500

    Allow default of compute zone

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ variable NODE_COUNT {}
 
 resource "google_container_cluster" "primary" {
   name               = "${var.CLUSTER_NAME}"
-  zone               = "${var.GOOGLE_COMPUTE_ZONE}"
   initial_node_count = "${var.NODE_COUNT}"
 
   node_config {

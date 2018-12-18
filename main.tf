6dc8981fbc19892e8d47aa604c591d3e5c33a324
Stephen Leyva <sleyva@staq.com>
2018-12-18 12:29:57 -0500

    Change Machine type and add required permissions

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 variable CLUSTER_NAME {}
 variable NODE_COUNT {}
 
+variable MACHINE_TYPE {
+  default = " n1-standard-1"
+}
+
 resource "google_container_cluster" "primary" {
   name               = "${var.CLUSTER_NAME}"
   initial_node_count = "${var.NODE_COUNT}"
 
   node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    machine_type = "${var.MACHINE_TYPE}"
+
     tags = ["environment", "${var.ENVIRONMENT}"]
   }
 }

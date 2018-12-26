3946777e8ba28ec040faebc9071ec73262f2a806
Stephen Leyva <sleyva@staq.com>
2018-12-26 10:50:18 -0500

    Add auto scaling
    * Additionally duplication between planning stages
      and the actual deploy found. I need to come up with a sane way to avoid this.
      Perhaps planning and deploy in the same stage?

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 variable CLUSTER_NAME {}
-variable NODE_COUNT {}
+variable MIN_NODE_COUNT {}
+variable MAX_NODE_COUNT {}
 
 variable MACHINE_TYPE {
   default = "n1-standard-1"
@@ -7,18 +8,29 @@ variable MACHINE_TYPE {
 
 resource "google_container_cluster" "primary" {
   name               = "${var.CLUSTER_NAME}"
-  initial_node_count = "${var.NODE_COUNT}"
+  initial_node_count = "${var.MIN_NODE_COUNT}"
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+  node_pool {
+    name = "default-pool"
 
-    machine_type = "${var.MACHINE_TYPE}"
+    autoscaling {
+      min_node_count = "${var.MIN_NODE_COUNT}"
+      max_node_count = "${var.MAX_NODE_COUNT}"
+    }
 
-    tags = ["environment", "${var.ENVIRONMENT}"]
+    node_config {
+      preemptible  = false
+      disk_size_gb = 100
+      machine_type = "${var.MACHINE_TYPE}"
+
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/compute",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+      ]
+
+      tags = ["environment", "${var.ENVIRONMENT}"]
+    }
   }
 }
